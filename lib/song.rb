805e84bc3ee12991a07a4f824393b6ca3e9da503
kevin lang <langkevina@gmail.com>
2019-06-10 15:20:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
     self.artist.save
     artist.add_song(self)
   end
-    
+
   def self.new_by_filename(file_name)
     song = Song.new(file_name)
     songArr = file_name.split(" - ")

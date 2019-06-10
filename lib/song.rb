0562d46ba71e0c78503872484e505cb1b3d4daac
kevin lang <langkevina@gmail.com>
2019-06-10 15:02:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,8 +6,6 @@ class Song
   def self.new_by_filename(file_name)
     song = Song.new(file_name)
     songArr = file_name.split(" - ")
-    song.name = songArr[1]
-    song.artist.name = songArr[0]
     song
   end
 end
